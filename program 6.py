2bff0480e9e6b7e4c1062c4c971629009fb2ebef
Aswin surendran <126644198+aswinashu12@users.noreply.github.com>
2023-08-04 12:52:06 +0530

    Add files via upload

---
+++ program 6.py
@@ -0,0 +1,6 @@
+#voting eligibility
+x=int(input("enter the age of the candidate  "))
+if x>=18:
+    print("the candidate is eligible to vote")
+else:
+    print("the candidate is not eligible to vote")
