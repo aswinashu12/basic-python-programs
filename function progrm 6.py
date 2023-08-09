d967129a16c1e401c4b8905720fc1de2d040866a
Aswin surendran <126644198+aswinashu12@users.noreply.github.com>
2023-08-09 13:01:34 +0530

    Add files via upload

---
+++ function progrm 6.py
@@ -0,0 +1,7 @@
+a=int(input("enter the 1st number"))
+b=int(input("enter the 2nd number"))
+c=int(input("enter the 3rd number"))
+def sum(a,b,c,d=10):
+    sum=a+b+c+d
+    return sum
+print(sum(a,b,c))    
